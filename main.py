9134eec826f117f480fb1748f5788e25dbaff478
Cute Boy <abdoulazizc867@gmail.com>
2025-07-01 17:05:55 +0000

    Print a pizza, use of recycleview

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from kivy.app import App
+from kivy.properties import ObjectProperty, StringProperty
 from kivy.uix.boxlayout import BoxLayout
 
 from models import Pizza
 
 
+class PizzaWidget(BoxLayout):
+    name = StringProperty()
+
 class MainWidget(BoxLayout):
+    recycleView = ObjectProperty(None)
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.pizzas = [
@@ -12,6 +18,12 @@ class MainWidget(BoxLayout):
             Pizza("Chorizo", "tomates, chorizo, parmesan", 11.2, False),
             Pizza("Calzone", "fromage, jambon, champignons, ", 10, False)
 
+        ]
+
+    def on_parent(self, widget, parent):
+        self.recycleView.data = [
+            {"name": "4 fromages"},
+            {"name": "Chorizo"}
         ]
 
 
